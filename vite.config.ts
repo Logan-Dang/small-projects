954c974ce9413e52e6c1d5ffd4cf5dd0d11763dc
Logan Dang <logandang100@gmail.com>
2025-10-02 17:06:50 -0700

    make build work

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/',
+  base: "/",
   plugins: [react()],
   server: {
     port: 5713,
     host: "localhost",
   },
-  assetsInclude: ["**/*.html"],
-})
+});
